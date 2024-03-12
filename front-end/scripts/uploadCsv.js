5f0e8c0da3e929e5789e34d7bf81d6fefb207c72
Grammar-Programmer <acmoz@iscte-iul.pt>
2024-03-12 16:49:27 +0000

    Adicionar documentação

--- front-end/scripts/uploadCsv.js
+++ front-end/scripts/uploadCsv.js
@@ -4,7 +4,14 @@ import axios from "axios";
 const localForm = document.getElementById("localUpload");
 localForm.addEventListener("submit", handleSubmit);
 
-/** @param {Event} event */
+/**
+ * Recebe um ficheiro .csv local ou o url de um ficheiro csv remoto.
+ *
+ * Se existir um .csv local e um url ao mesmo tempo vai dar prioridade ao ficheiro local,
+ * caso contrário faz um chamada a API para ir buscar o ficheiro .csv.
+ *
+ * @param {Event} event
+ */
 async function handleSubmit(event) {
   event.preventDefault(); // Colocar comentário para testar
   const form = event.currentTarget;
@@ -14,15 +21,22 @@ async function handleSubmit(event) {
     //Example URL:https://raw.githubusercontent.com/AlexandreMilharado/filesToUpload/main/HorarioDeExemplo.csv
     axios
       .post("http://localhost:3001/uploadHorario", {
-        file: remoteFile,
+        url: remoteFile,
       })
       .then((r) => formatCsv(r.data.csvData))
       .catch((e) => console.log(JSON.stringify(e.response.data)));
   } else {
-    format(localFile);
+    formatCsv(formatToString(localFile));
   }
 }
 
+/**
+ * Função que transforma os inputs com os respetivos values num JSON.
+ *
+ * Exemplo: {"localFile" : localFile.value,"remoteFile": remoteFile.value }
+ * @param {FormData} formData
+ * @returns JSON {...}
+ */
 function formDataToJson(formData) {
   const json = {};
   formData.forEach((value, key) => {
@@ -31,6 +45,15 @@ function formDataToJson(formData) {
   return json;
 }
 
+/**
+ * Verifica se precisa de fazer a chamada a API para fazer download do ficheiro.
+ *
+ * Propriedades: Nesta função dá-se prioridade ao ficheiro local, ou seja, se ambos os campos
+ * estiverem preenchidos apenas vai buscar o ficheiro local.
+ * @param {File} localFile
+ * @param {String} remoteFile
+ * @returns Boolean
+ */
 function needToDownloadCsv(localFile, remoteFile) {
   return (
     localFile?.type == "application/octet-stream" &&
@@ -39,11 +62,23 @@ function needToDownloadCsv(localFile, remoteFile) {
   );
 }
 
-async function format(localFile) {
+/**
+ * Recebe um ficheiro e transforma-o em string.
+ * @param {File} localFile
+ * @returns Ficheiro em String
+ */
+async function formatToString(localFile) {
   const text = await localFile.text();
-  formatCsv(text);
+  return text;
 }
 
+/**
+ * Recebe em texto um ficheiro .csv e transforma-o num array.
+ *
+ * Depois imprime na consola o array. //TODO mudar funcionalidade
+ * Por exemplo: colocar numa variável global.
+ * @param {String} text
+ */
 function formatCsv(text) {
   const newArr = text.split("\n").map((linha) => linha.split(";"));
   console.log(newArr);
