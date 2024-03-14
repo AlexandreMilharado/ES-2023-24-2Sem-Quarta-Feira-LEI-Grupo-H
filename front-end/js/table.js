6fefd208dc014f074e70798f1924583801f9c5a6
olpol29 <olpolischuk29@gmail.com>
2024-03-14 19:58:11 +0000

    Implementation of hide column function

--- front-end/js/table.js
+++ front-end/js/table.js
@@ -62,21 +62,38 @@ var table = new Tabulator("#HorarioPrincipal", {
     ],
 });
 
-
-
 //Iniciar a tabela com os filtros desligados (estao ligados por default)
 let horario = document.getElementById("HorarioPrincipal")
 horario.setAttribute("filters", "off")
-
 
 //Inserir Filter Toggle Button no paginator da tabela pre-feita
 let paginators = document.getElementsByClassName("tabulator-paginator");
 console.log(paginators)
 let filterToggleButton = document.createElement("button")
 filterToggleButton.className = "tabulator-filter-toggle-button"
+let editToggleButton=document.createElement("button");
+editToggleButton.className="tabulator-edit-toggle-button"
+editToggleButton.setAttribute("toggled","off");
 for (var i = 0; i < paginators.length; i++) {
     paginators.item(i).prepend(filterToggleButton);
     filterToggleButton.addEventListener("click", ()=> toggleFilter());
+    paginators.item(i).prepend(editToggleButton);
+    editToggleButton.addEventListener("click",()=>toggleEdit(editToggleButton));
+}
+//Adiciona a todas as colunas o botão de esconder a respetiva coluna 
+addHidenButtonsToColumns();
+function addHidenButtonsToColumns(){
+    let columns=document.getElementsByClassName("tabulator-col");
+    console.log(columns.length);
+    for (let i = 0; i < columns.length; i++) {
+        // columns[i].classList.add("tabulator-edit");
+        const button=document.createElement("button");
+        button.className="tabulator-hideColumn-toggle-button";
+        button.textContent="Esconder";
+        columns[i].getElementsByClassName("tabulator-col-content")[0].appendChild(button);
+        const nameColumn=columns[i].getAttribute("tabulator-field");
+        button.addEventListener("click", ()=>  hideColumn(columns[i],nameColumn));
+    }
 }
 
 //funcao que liga/desliga os filtros
@@ -88,5 +105,33 @@ function toggleFilter(){
         }else{
             table.setAttribute("filters","off");
         }
-        
+}
+//Esta funcao é resposnavel por esconder/mostrar os botes de esconder colunas
+function toggleEdit(){
+    if(editToggleButton.getAttribute("toggled")=="on"){
+        console.log("Estava ligado passou a estar desligado");
+        editToggleButton.setAttribute("toggled","off");
+    }else{
+        console.log("Estava desligado passou a estar ligado");
+        editToggleButton.setAttribute("toggled","on");
+    } 
+}
+//Esta funcao é resposnavel por esconder as colunas
+function hideColumn(colum,nameColumn){
+    table.hideColumn(nameColumn);
+    addHidedColumns(colum,nameColumn);
+}
+//Esta funcao é resposnavel por colocar as colunas escondias ao lado da tabela principal de forma que o user
+//consiga voltar a metelas no lugar caso o deseja
+function addHidedColumns(column,nameColumn){
+    const list=document.getElementById("HidedColumns").getElementsByTagName("ul")[0];
+    const button = document.createElement('button');
+    button.className="tabulator-hidedColumn-toggle-button";
+    const tileColumn=column.getElementsByClassName("tabulator-col-title")[0].textContent;
+    button.textContent =tileColumn;
+    list.appendChild(button);
+    button.addEventListener("click", ()=>{
+        button.remove();
+        table.showColumn(nameColumn);
+    });
 }
