2d2a65c440266ea275c1ebaeed769a31d3b7c9d8
Grammar-Programmer <acmoz@iscte-iul.pt>
2024-04-12 16:35:27 +0100

    Adding tests to cover all dates.ts File

--- front-end/tests/dates.test.ts
+++ front-end/tests/dates.test.ts
@@ -32,6 +32,11 @@ describe("dateStringFormatCToDate", () => {
     const result: Date = dateStringFormatCToDate("12/01/23") as Date;
     expect(result).toStrictEqual(new Date("2023/01/12"));
   });
+
+  it('devolve null caso caso seja passado a string com ""', () => {
+    const result: Date = dateStringFormatCToDate("") as Date;
+    expect(result).toBe(null);
+  });
 });
 
 describe("getWeekNumber", () => {
@@ -48,53 +53,68 @@ describe("getWeekNumber", () => {
   });
 });
 
-
-
-
 describe("getSemesterWeekNumber", () => {
   it("devolve EMPTY_DATA se for passada uma data inválida", () => {
     expect(
-      getSemesterWeekNumber(
-        new Date("2024/12/50"),
-        {
-          2023:{first:new Date(2023, 8, 1), second:new Date(2024, 0, 30)}
-        }        
-      )
+      getSemesterWeekNumber(new Date("2024/12/50"), {
+        2023: { first: new Date(2023, 8, 1), second: new Date(2024, 0, 30) },
+      })
     ).toBe(EMPTY_DATA);
   });
 
   it("devolve  número da semana correto para uma data do segundo semestre", () => {
     expect(
-      getSemesterWeekNumber(
-        new Date("2024/2/13"),
-        {
-          2023:{first:new Date(2023, 8, 1), second:new Date(2024, 0, 30)}
-        }    
-      )
+      getSemesterWeekNumber(new Date("2024/2/13"), {
+        2023: { first: new Date(2023, 8, 1), second: new Date(2024, 0, 30) },
+      })
     ).toBe(3);
+  });
+
+  it("devolve  número da semana correto para uma data do primeiro semestre antes do ano acabar", () => {
+    expect(
+      getSemesterWeekNumber(new Date("2023/9/9"), {
+        2023: { first: new Date(2023, 8, 1), second: new Date(2024, 0, 30) },
+      })
+    ).toBe(2);
   });
 
   it("devolve  número da semana correto para uma data do primeiro semestre", () => {
     expect(
-      getSemesterWeekNumber(
-        new Date("2024/1/13"),
-        {
-          2023:{first:new Date(2023, 8, 1), second:new Date(2024, 0, 30)}
-        }    
-      )
+      getSemesterWeekNumber(new Date("2024/1/13"), {
+        2023: { first: new Date(2023, 8, 1), second: new Date(2024, 0, 30) },
+      })
     ).toBe(20);
   });
 });
 
-describe('getSemesterStarts', () => {
+describe("getSemesterStarts", () => {
   it("devolve um objecto com a primeira aula do primeiro semestre de 2018/2019 e segundo semestre 2017/2018", () => {
     expect(
-      getSemesterStarts(["3/12/2018","8/12/2018","1/1/2019","5/9/2018", "4/2/2018", "10/2/2018"])
-    ).toStrictEqual({2018:{first:new Date(2018,8,5)} , 2017:{second:new Date(2018,1,4)}})
-  }) 
+      getSemesterStarts([
+        "3/12/2018",
+        "8/12/2018",
+        "1/1/2019",
+        "5/9/2018",
+        "4/2/2018",
+        "10/2/2018",
+      ])
+    ).toStrictEqual({
+      2018: { first: new Date(2018, 8, 5) },
+      2017: { second: new Date(2018, 1, 4) },
+    });
+  });
   it("devolve um objecto com a primeira aula do primeiro semestre de 2018/2019 e segundo semestre 2017/2018 com datas ", () => {
     expect(
-      getSemesterStarts(["3/13/2018","2018/12/08","1/1/2019","5/9/2018", "10/2/2018"])
-    ).toStrictEqual({2018:{first:new Date(2018,8,5)} , 2017:{second:new Date(2018,1,10)}})
-  }) 
-})
+      getSemesterStarts([
+        "3/13/2018",
+        "2018/12/08",
+        "1/1/2019",
+        "5/9/2018",
+        "10/2/2018",
+      ])
+    ).toStrictEqual({
+      2018: { first: new Date(2018, 8, 5) },
+      2017: { second: new Date(2018, 1, 10) },
+    });
+  });
+});
